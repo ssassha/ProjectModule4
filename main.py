fdce6280c7b695cadb6cdecccd0d062b13f6a38b
Sasha <al_ganin@mail.ru>
2023-06-13 17:47:28 +0300

    initial

---
+++ main.py
@@ -0,0 +1,21 @@
+# def strcounter(s):
+#     for sym in set(s):
+#         counter = 0
+#         for sub_sym in s:
+#             if sym == sub_sym:
+#                 counter += 1
+#         print(sym, counter)
+#
+#
+# strcounter('aaabbbbccd')
+
+def strcounter(s):
+    syms_counter = { }
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+    print(syms_counter)
+
+    for sym, count in syms_counter.items():
+        print(sym, count)
+
+strcounter('aaabbbbccd')
